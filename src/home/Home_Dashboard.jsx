db58f5b815cb0abafafa0d249c3b19305de0e186
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-24 03:06:19 +0700

    feat: add time feature

--- src/home/Home_Dashboard.jsx
+++ src/home/Home_Dashboard.jsx
@@ -1,11 +1,26 @@
 import "./Home_Dashboard.css";
+import { useEffect, useState } from "react";
 
 function Home_Dashboard() {
+  const currentDate = new Date();
+  const [thdate, setDate] = useState("");
+  const thMonthNames = [
+    "ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.",
+    "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค."
+  ];
+  
+  useEffect(() => {
+    const day = currentDate.getDate();
+    const monthIndex = currentDate.getMonth();
+    const thaiMonth = thMonthNames[monthIndex];
+    setDate(`${day} ${thaiMonth}`);
+  },[]);
+  
   return (
     <div className="main-homeDashboard">
       <div>
         <h4>รายการเครื่องใช้ไฟฟ้าภายในครัวเรือน</h4>
-        <p style={{ color: "#e9714f" }}>วันนี้, 21 พ.ย.</p>
+        <p style={{ color: "#e9714f" }}>{`วันนี้, ${thdate}`}</p>
       </div>
       <h4 className="detail">รวมการใช้พลังงาน 63.53 หน่วย</h4>
       <div className="homeDashboard">แผนภูมิ</div>
