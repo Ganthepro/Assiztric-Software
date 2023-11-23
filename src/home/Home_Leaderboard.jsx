cac737d9d3d2cd51d56dc52e047eb3a9de2b1c7f
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-23 15:47:27 +0700

    feat: update from last commit

--- src/home/Home_Leaderboard.jsx
+++ src/home/Home_Leaderboard.jsx
@@ -6,7 +6,9 @@ function Home_Leaderboard() {
         <div className="main-homeLeaderboard">
             <h4 style={{marginBottom: "10px"}}>การใช้งาน</h4>
             <div className="homeLeaderboard">
-                <Leaderboard_Data />
+                <Leaderboard_Data data="100px" />
+                <Leaderboard_Data data="150px" />
+                <Leaderboard_Data data="190px" isLast={true} />
             </div>
         </div>
     )
--- src/templent/Leaderboard_Data.jsx
+++ src/templent/Leaderboard_Data.jsx
@@ -1,12 +1,31 @@
-import "./Leaderboard_Data.css"
-import TV_Logo from "../assets/Television_Icon.svg"
+import "./Leaderboard_Data.css";
+import TV_Logo from "../assets/Television_Icon.svg";
+import RightArrow from "../assets/RightArrow.svg";
 
-function Leaderboard_Data() {
-    return (
-        <div className="main-leaderboardData">
-            <img src={TV_Logo}></img>
+function Leaderboard_Data(props) {
+  return (
+    <>
+      <div className="main-leaderboardData">
+        <img src={TV_Logo} style={{ width: "40px" }} />
+        <div className="leaderboardData">
+            <div style={{flexDirection:"column",justifyContent: "space-around",display:"flex",alignItems:"unset",height:"100%",width:"100%"}}>
+                <p>โทรทัศน์</p>
+                <div
+                    style={{
+                    height: "5px",
+                    backgroundColor: "#A2A2A2",
+                    width: "95%",
+                    borderRadius: "10px",
+                    }}
+                ></div>
+            </div>
+            <p style={{height:"fit-content",whiteSpace:"nowrap"}}>1 ชม. 3 นาที</p>    
         </div>
-    )
+        <img src={RightArrow} alt="" style={{height:"17px"}} />
+      </div>
+      {props.isLast ? null : <hr />}
+    </>
+  );
 }
 
-export default Leaderboard_Data
+export default Leaderboard_Data;
