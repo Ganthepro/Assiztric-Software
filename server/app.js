84125b50ee6ad856c691f9dd274402f41c74307c
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-25 02:00:50 +0700

    new

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.get("/auth", async (req, res) => {
   const data = new URLSearchParams({
     grant_type: "authorization_code",
     code: code,
-    redirect_uri: `http://${process.env.HOST}/`,
+    redirect_uri: `http://${import.meta.env.VITE_HOST}/`,
     client_id: process.env.CLIENT_ID,
     client_secret: process.env.CLIENT_SECRET,
   });
