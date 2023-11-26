d76928e59056eb27fa9a7a5814eb39091c1b0e38
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-26 22:03:53 +0700

    new

--- server/index.js
+++ server/index.js
@@ -2,6 +2,28 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const db = require("./db");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+const Schema = mongoose.Schema;
+const User = mongoose.model("User", userSchema, "users");
+
+const userSchema = new Schema({
+  userId: String,
+  displayName: String,
+  pictureUrl: String,
+});
 
 app.use(express.json());
 app.use(cors());
@@ -17,9 +39,20 @@ async function middleware(req, res, next) {
 }
 
 app.post("/auth", middleware, async (req, res) => {
-  if (db.findUser(req.body.userId)) return res.status(200).send(db.findUser(req.body.userId, true));
-  db.addUser(req.body);
-  console.log(db.findUser(req.body.userId));
+  // if (db.findUser(req.body.userId)) return res.status(200).send(db.findUser(req.body.userId, true));
+  // db.addUser(req.body);
+  // console.log(db.findUser(req.body.userId));
+  
+  User.findOne({ userId: req.body.userId })
+  .then((user) => {
+    if (!user) return res.status(200).send("user not found");
+    // return user; // Send the user data as JSON
+    return res.status(200).send(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    return false;
+  });
 });
 
 app.listen(5500, () => {
