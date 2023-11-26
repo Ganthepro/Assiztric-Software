5b08b8a2e1e21b4ae70f60bbd5a8fac2051b2836
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-26 23:58:58 +0700

    new

--- server/index.js
+++ server/index.js
@@ -52,13 +52,13 @@ app.post("/auth", middleware, async (req, res) => {
     const data = req.body;
     const user = await User.findOne({ userId: req.body.userId });
     if (!user) {
-      console.log("User not found!");
       const newUser = new User({
         userId: data.userId,
         displayName: data.displayName,
         pictureUrl: data.pictureUrl,
       });
-      newUser
+      console.log(newUser);
+      await newUser
         .save()
         .then((result) => {
           console.log("New user saved:", result);
@@ -66,6 +66,7 @@ app.post("/auth", middleware, async (req, res) => {
         })
         .catch((err) => {
           console.error("Error saving user:", err);
+          return res.json(err);
         });
     }
     return res.status(200).json(user); // Send the user data as JSON
