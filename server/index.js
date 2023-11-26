ace72ab6fe8388351f36cfe34a329e0ee807c5cd
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-26 22:12:22 +0700

    new

--- server/index.js
+++ server/index.js
@@ -5,10 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
