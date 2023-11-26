318eb2b7e9087d35693d75807676d14acfecf201
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-26 21:16:05 +0700

    new

--- server/index.js
+++ server/index.js
@@ -9,19 +9,41 @@ app.use(cors());
 async function middleware(req, res, next) {
   const token = req.headers["token"];
   if (!token) return res.status(401).send("Access denied, token missing");
-  const respone = await fetch(`https://api.line.me/oauth2/v2.1/verify?access_token=${token}`, {
-    method: "GET"
-  });
+  const respone = await fetch(
+    `https://api.line.me/oauth2/v2.1/verify?access_token=${token}`,
+    {
+      method: "GET",
+    }
+  );
   if (!respone.ok) return res.status(401).send("Invalid token");
   next();
 }
 
 app.post("/auth", middleware, async (req, res) => {
   console.log(req.body);
-  console.log(db.findUser(req.body.userId, true));
-  if (db.findUser(req.body.userId, true)) return res.status(200).send("User already exists");
-  db.addUser(req.body);
-  console.log(db.findUser(req.body.userId, false));
+
+  try {
+    const userExists = await db.findUser(req.body.userId, true);
+    console.log(userExists); // This will log the boolean value
+
+    if (userExists) {
+      return res.status(200).send("User already exists");
+    }
+
+    await db.addUser(req.body);
+
+    // Check user after adding
+    const addedUser = await db.findUser(req.body.userId, false);
+    console.log(addedUser); // This will log the user data as JSON
+
+    // Send response or perform other operations based on addedUser if needed
+
+    // Send a success response
+    return res.status(200).send("User added successfully");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Error processing request");
+  }
 });
 
 app.listen(5500, () => {
