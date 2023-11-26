a7da655e2f873de30ea5f532c20b774f47b75b1c
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-26 22:36:22 +0700

    new

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cors());
 async function middleware(req, res, next) {
   const token = req.headers["token"];
   if (!token) return res.status(401).send("Access denied, token missing");
-
+  console.log(token);
   try {
     const response = await fetch(
       `https://api.line.me/oauth2/v2.1/verify?access_token=${token}`,
