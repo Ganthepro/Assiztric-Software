986dc32b0c7d84101558e1fded94d47fbd8ace8b
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-26 21:45:04 +0700

    new

--- server/index.js
+++ server/index.js
@@ -2,6 +2,29 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 // const db = require("./db");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  userId: String,
+  displayName: String,
+  pictureUrl: String,
+});
+
+const User = mongoose.model("User", userSchema, "users");
 
 app.use(express.json());
 app.use(cors());
