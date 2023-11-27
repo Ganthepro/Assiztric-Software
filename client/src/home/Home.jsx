b1094820f24971a3f3f727b68b874c7f94abc990
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-27 19:29:31 +0700

    new

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -4,94 +4,18 @@ import Header from "../templent/Header";
 import Home_Dashboard from "./Home_Dashboard";
 import Home_Leaderboard from "./Home_Leaderboard";
 import Blank from "../templent/Blank";
-import { useEffect, useRef, useState } from "react";
-import Cookies from "js-cookie";
-import liff from "@line/liff";
 
 export function Home(props) {
-  const [token, setToken] = useState(null);
-  const [profiles, setProfiles] = useState(null);
-
-  // async function getProfile() {
-  //   try {
-  //     const response = await fetch('https://api.line.me/v2/profile', {
-  //       headers: {
-  //         'Authorization': `Bearer ${Cookies.get("token")}`
-  //       },
-  //       method: 'GET'
-  //     });
-  //     const profile = response.data;
-  //     const profileId = profile.userId; // Extract the profile ID from the response
-  //     return console.log(profile);
-  //   } catch (error) {
-  //     console.error('Error fetching profile ID:', error);
-  //     throw new Error('Failed to fetch profile ID');
-  //   }
-  // }
-  
-  async function login() {
-    try {
-      await liff.init({ liffId: "2001224573-pxK3m42V", withLoginOnExternalBrowser:true }); // Replace with your LIFF ID
-      if (!liff.isLoggedIn()) {
-        liff.login();
-      } else {
-        const accessToken = liff.getAccessToken();
-        const profile = await liff.getProfile();
-        if (accessToken) {
-          console.log('Access Token:', accessToken);
-          Cookies.set("token", accessToken, { expires: 1 });
-          Cookies.set("userId", profile.userId, { expires: 1 });
-          setToken(accessToken);
-          fetch(`https://assiztric-software.vercel.app/auth`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "token": accessToken,
-            },
-            body: JSON.stringify({
-              userId: Cookies.get("userId"),
-              displayName: profile.displayName,
-              pictureUrl: profile.pictureUrl
-            }),
-          })
-            .then((res) => res.json())
-            .then((data) => {
-              console.log(data);
-              setProfiles([data.displayName, data.pictureUrl]);
-          })
-        } else {
-          console.error("Access token not available");
-        }
-      }
-    } catch (error) {
-      console.error("LIFF initialization failed", error);
-    }
-  }
-
-  useEffect(async () => {
-    login();
-  }, []);
-
   return (
     <div className="main-home">
-      {profiles != null ? (
-        <>
-          <Header profile={profiles[0]} profilePic={profiles[1]} />
-          <div className="body-home">
-            <h1 style={{ marginTop: "15px" }}>
-             Home
-            </h1>
-            <Home_Dashboard />
-            <Home_Leaderboard />
-          </div>
-          <Blank />
-          <Nav id={props.id} />
-        </>
-      ) : (
-        <div className="main-landing">
-          <h1>Waiting for Line...</h1>
-        </div>
-      )}
+      <Header />
+      <div className="body-home">
+        <h1 style={{ marginTop: "15px" }}>Home</h1>
+        <Home_Dashboard />
+        <Home_Leaderboard />
+      </div>
+      <Blank />
+      <Nav id={props.id} />
     </div>
   );
 }
--- client/src/templent/Header.jsx
+++ client/src/templent/Header.jsx
@@ -1,14 +1,63 @@
 import './Header.css'
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import liff from "@line/liff";
 
 function Header(props) {
+  const [profiles, setProfiles] = useState(null);
+  
+  async function login() {
+    try {
+      await liff.init({ liffId: "2001224573-pxK3m42V", withLoginOnExternalBrowser:true }); // Replace with your LIFF ID
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      } else {
+        const accessToken = liff.getAccessToken();
+        const profile = await liff.getProfile();
+        if (accessToken) {
+          console.log('Access Token:', accessToken);
+          Cookies.set("token", accessToken, { expires: 1 });
+          Cookies.set("userId", profile.userId, { expires: 1 });
+          setToken(accessToken);
+          fetch(`https://assiztric-software.vercel.app/auth`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "token": accessToken,
+            },
+            body: JSON.stringify({
+              userId: Cookies.get("userId"),
+              displayName: profile.displayName,
+              pictureUrl: profile.pictureUrl
+            }),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+              setProfiles([data.displayName, data.pictureUrl]);
+              props.func();
+          })
+        } else {
+          console.error("Access token not available");
+        }
+      }
+    } catch (error) {
+      console.error("LIFF initialization failed", error);
+    }
+  }
+
+  useEffect(async () => {
+    login();
+  }, []);
+
   return (
     <div className="main-header">
       <div className="box-left" style={{width:"80%"}}>
-        <h1 style={{textOverflow:"ellipsis",whiteSpace:"nowrap",overflow:"hidden"}}>{props.profile}</h1>
+        <h1 style={{textOverflow:"ellipsis",whiteSpace:"nowrap",overflow:"hidden"}}>{profiles != null ? profiles[0] : "Loading..."}</h1>
         <h4 style={{ color: "#E9714F" }}>ค่าใช้ไฟฟ้า 1,535.65</h4>
       </div>
       <div className="profile">
-        <img src={props.profilePic} alt="profile-pic" />
+        <img src={profiles[1]} alt="profile-pic" />
       </div>
     </div>
   )
