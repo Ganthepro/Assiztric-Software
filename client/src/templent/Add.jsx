20bbe82e742931dc79552f0faed6338fa15e77ee
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-28 23:56:53 +0700

    new

--- client/src/templent/Add.jsx
+++ client/src/templent/Add.jsx
@@ -1,17 +1,28 @@
 import './Add.css';
 import addPic from '../assets/add.svg';
 import OverlayAdd from './OverlayAdd';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function Add() {
     const [show, setShow] = useState(false);
+    const add = useRef(null);
+
+    function onTouchStart() {
+        add.current.style.backgroundColor = '#E9544F';
+        add.current.style.borderRadius = '50%';
+    }
+
+    function onTouchEnd() {
+        add.current.style.backgroundColor = '#E9714F';
+        add.current.style.borderRadius = '50%';
+    }
 
     return (
         <div className='main-add'>
             {
                 show ? <OverlayAdd setShow={setShow} /> : null
             }
-            <div className='button-add' onClick={() => {setShow(true);console.log(show)}}>
+            <div className='button-add' onClick={() => {setShow(true);console.log(show)}} onTouchStart={onTouchStart} onTouchEnd={onTouchEnd} ref={add}>
                 <img src={addPic} alt="add" />
             </div>
         </div>
