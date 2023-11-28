fb6819d73d662346a9c8a9d3936d693128d6cb05
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-29 00:49:33 +0700

    new

--- client/src/templent/Input.jsx
+++ client/src/templent/Input.jsx
@@ -1,17 +1,27 @@
-import './input.css'
+import React, { forwardRef } from "react";
+import "./input.css";
 
-function Input(props) {
-    return (
-        <div className='main-input'>
-            <label>{props.title}</label>      
-            {
-                props.isBig ? 
-                <textarea placeholder={props.placeholder} rows="4" cols="50" ref={props.ref} />
-                :
-                <input type="text" placeholder={props.placeholder} size="50" ref={props.ref} />
-            }      
-        </div>
-    )
-}
+const Input = forwardRef((props, ref) => {
+  return (
+    <div className="main-input">
+      <label>{props.title}</label>
+      {props.isBig ? (
+        <textarea
+          placeholder={props.placeholder}
+          rows="4"
+          cols="50"
+          ref={ref}
+        />
+      ) : (
+        <input
+          type="text"
+          placeholder={props.placeholder}
+          size="50"
+          ref={ref}
+        />
+      )}
+    </div>
+  );
+});
 
-export default Input
+export default Input;
