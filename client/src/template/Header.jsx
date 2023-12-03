40715877bda452f294d7fc14c4dd02aa8d73ca14
Tamtikorn <gan_zaza_12@hotmail.com>
2023-12-03 13:06:27 +0700

    new

--- client/src/template/Header.jsx
+++ client/src/template/Header.jsx
@@ -1,56 +1,6 @@
 import './Header.css'
-import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
-// import liff from "@line/liff";
 
 function Header(props) {
-  // const [profiles, setProfiles] = useState(null);
-  
-  // async function login() {
-  //   try {
-  //     await liff.init({ liffId: "2001224573-pxK3m42V", withLoginOnExternalBrowser:true }); // Replace with your LIFF ID
-  //     Cookies.set("isLogin", false, { expires: 1 })
-  //     if (!liff.isLoggedIn()) {
-  //       liff.login();
-  //     } else {
-  //       const accessToken = liff.getAccessToken();
-  //       const profile = await liff.getProfile();
-  //       if (accessToken) {
-  //         Cookies.set("token", accessToken, { expires: 1 });
-  //         Cookies.set("userId", profile.userId, { expires: 1 });
-  //         Cookies.set("pictureUrl", profile.pictureUrl, { expires: 1 });
-  //         Cookies.set("displayName", profile.displayName, { expires: 1 });
-  //         Cookies.set("isLogin", true, { expires: 1 })
-  //         fetch(`https://assiztric-software.vercel.app/auth`, {
-  //           method: "POST",
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //             "token": accessToken,
-  //           },
-  //           body: JSON.stringify({
-  //             userId: Cookies.get("userId"),
-  //             displayName: profile.displayName,
-  //             pictureUrl: profile.pictureUrl
-  //           }),
-  //         })
-  //           .then((res) => res.json())
-  //           .then((data) => {
-  //             setProfiles([data.displayName, data.pictureUrl]);
-  //         })
-  //       } else {
-  //         console.error("Access token not available");
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.error("LIFF initialization failed", error);
-  //   }
-  // }
-
-  // useEffect(async () => {
-  //   if (Cookies.get("userId") == "" || Cookies.get("userId") == undefined || Cookies.get("userId") == null) props.loginFunc();
-  //   else setProfiles([Cookies.get("displayName"), Cookies.get("pictureUrl")]);
-  // }, []);
-
   return (
     <div className="main-header">
       <div className="box-left" style={{width:"80%"}}>
