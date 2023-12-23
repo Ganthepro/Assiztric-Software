21454522db539669437d26d0c1beb439698b6475
Tamtikorn <gan_zaza_12@hotmail.com>
2023-12-23 13:14:26 +0700

    bew

--- server/index.js
+++ server/index.js
@@ -113,7 +113,7 @@ app.post("/addApplianceDataHistory", middleware, async (req, res) => {
     return output;
   }
   try {
-    await fetch("https://2565-2001-fb1-aa-46eb-68af-2186-97fb-6881.ngrok-free.app", {
+    await fetch("https://2565-2001-fb1-aa-46eb-68af-2186-97fb-6881.ngrok-free.app/prediction", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
