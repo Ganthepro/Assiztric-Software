63da2d05ee27d739aab665d25f51e3d80af963ab
Tamtikorn <gan_zaza_12@hotmail.com>
2023-12-26 20:26:39 +0700

    new

--- client/src/template/OverlayAdd.jsx
+++ client/src/template/OverlayAdd.jsx
@@ -19,7 +19,7 @@ function OverlayAdd(props) {
       UsageBehavior: descriptionRef.current.value,
     };
     try {
-      const response = await fetch(`http://localhost:5500/addApplianceData`, {
+      const response = await fetch(`https://assiztric-software.vercel.app/addApplianceData`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
