94234eacf2b15214166ac9200292b7de60e1363c
Tamtikorn <gan_zaza_12@hotmail.com>
2023-12-27 20:49:06 +0700

    new

--- server/index.js
+++ server/index.js
@@ -90,13 +90,6 @@ const ApplianceDataHistory = mongoose.model(
 
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 async function resetData() {
   const existingDocument = await ApplianceDataHistory.findOne({});
