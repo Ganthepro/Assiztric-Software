5ca6b4eb023bbdff5e1ad22c9805b3253381f93b
Tamtikorn <gan_zaza_12@hotmail.com>
2023-12-27 21:00:42 +0700

    new

--- server/index.js
+++ server/index.js
@@ -88,13 +88,8 @@ const ApplianceDataHistory = mongoose.model(
   "applianceDataHistory"
 );
 
-const corsOptions = {
-  origin: 'https://assiztric.vercel.app',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 async function resetData() {
   const existingDocument = await ApplianceDataHistory.findOne({});
