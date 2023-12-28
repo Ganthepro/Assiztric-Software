dd099be257943b6554a2976913dea5d5cb379aa2
Tamtikorn <gan_zaza_12@hotmail.com>
2023-12-28 19:36:33 +0700

    new

--- server/index.js
+++ server/index.js
@@ -457,7 +457,7 @@ app.get("/getPredictData/:userId", middleware, async (req, res) => {
       date.setDate(date.getDate() - i);
       dates.push(date.toISOString().split('T')[0]);
     }
-    dates[-1] = 'Today'
+    dates[dates.length - 1] = 'Today'
     return dates;
   }
   try {
