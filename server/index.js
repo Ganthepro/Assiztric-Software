be181fc5a68d996cc714df0e5a0b892ce3104d08
Tamtikorn <gan_zaza_12@hotmail.com>
2023-12-28 20:17:23 +0700

    new

--- server/index.js
+++ server/index.js
@@ -282,9 +282,9 @@ app.post("/addApplianceDataHistory", middleware, async (req, res) => {
                       rv[result.Types[j]] = arr[i][j];
                     rs.push(rv);
                   }
+                  console.log(rs)
                   return rs;
                 }
-                console.log(await toObject(result.timeOfUsege));
                 fetch(
                   "https://2a69-2001-fb1-ab-a967-11f9-3e1b-4531-bcf7.ngrok-free.app/notification",
                   {
