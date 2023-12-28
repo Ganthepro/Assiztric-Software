ada19132388c1716ed3e49ec706a299c27e9973f
Tamtikorn <gan_zaza_12@hotmail.com>
2023-12-28 13:48:25 +0700

    new

--- server/index.js
+++ server/index.js
@@ -369,6 +369,7 @@ app.get("/getPredictData/:userId", middleware, async (req, res) => {
         : data.meanPowerStack;
     const timeWeek = data.times.length > 10080 ? data.times.slice(-10080) : data.times;
     const types = data.Types;
+    console.log("Predict data found");
     for (let i = 0; i < powerDistributionStackDay.length; i++) 
       if (powerDistributionStackDay[i].length < active.length) powerDistributionStackDay[i].push(0);
     for (let i = 0; i < powerDistributionStackWeek.length; i++)
