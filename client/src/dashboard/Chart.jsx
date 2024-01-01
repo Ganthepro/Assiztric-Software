13c3c3eb558f25ab6992d270e3bd06d4b28b766f
Tamtikorn <gan_zaza_12@hotmail.com>
2024-01-01 19:55:49 +0700

    new

--- client/src/dashboard/Chart.jsx
+++ client/src/dashboard/Chart.jsx
@@ -187,8 +187,8 @@ function Chart(props) {
           datasets: datasets,
         });
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(async (err) => {
+        console.log("Test")
         const cookies = document.cookie.split(";");
         for (let i = 0; i < cookies.length; i++) {
           const cookie = cookies[i];
