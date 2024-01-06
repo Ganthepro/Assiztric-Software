4ada41867b9da15cb9a17cc8a44178d518165a71
Tamtikorn <gan_zaza_12@hotmail.com>
2024-01-06 21:47:15 +0700

    new

--- client/src/dashboard/Chart.jsx
+++ client/src/dashboard/Chart.jsx
@@ -116,7 +116,7 @@ function Chart(props) {
         if (props.watt != null || props.watt != undefined)
           props.watt(data.totalWatt);
         if (props.cost != null || props.cost != undefined)
-          props.cost(data.totalCost);
+          props.cost(data.totalCost.toFixed(2));
         console.log(data.totalCost);
         let datasets = [];
         const powerDistributionStack = [];
--- server/index.js
+++ server/index.js
@@ -433,7 +433,6 @@ app.get("/getPredictData/:userId", middleware, async (req, res) => {
     const timeWeek = getPastSevenDays();
     const types = data.Types;
     const totalCost = data.totalCost;
-    console.log(totalCost.toFixed(2))
     for (let i = 0; i < powerDistributionStackDay.length; i++)
       if (powerDistributionStackDay[i].length < active.length)
         powerDistributionStackDay[i].push(0);
